5c25d48f98452e59414db49f55cbf225fa3d2ded
Ivan Stipić <ivanstipic2020@gmail.com>
2024-10-23 14:06:07 +0200

    Prvi file koji pusham

---
+++ src/main/java/com/example/predavanje_1/Predavanje1Application.java
@@ -0,0 +1,12 @@
+package com.example.predavanje_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Predavanje1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Predavanje1Application.class, args);
+    }
+}
---
+++ src/test/java/com/example/predavanje_1/Predavanje1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.predavanje_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Predavanje1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
