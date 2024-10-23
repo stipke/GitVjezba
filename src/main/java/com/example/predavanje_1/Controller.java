58dd3484ee0766780708e8636267d58bfdbfdf1e
Ivan Stipić <ivanstipic2020@gmail.com>
2024-10-23 14:48:57 +0200

    Izmjena druga

--- src/main/java/com/example/predavanje_1/Controller.java
+++ src/main/java/com/example/predavanje_1/Controller.java
@@ -7,7 +7,7 @@ public class Controller {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Pozdrav Andrija! ivan je";
+        return "Pozdrav Andrija! ivan je, evo me opet";
 
     }
 }
