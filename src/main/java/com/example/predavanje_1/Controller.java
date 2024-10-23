e568378f174ceff1c320dd1d9bd2e4447cae23c4
Ivan Stipić <ivanstipic2020@gmail.com>
2024-10-23 14:42:49 +0200

    Drugi put da comitam

---
+++ src/main/java/com/example/predavanje_1/Controller.java
@@ -0,0 +1,13 @@
+package com.example.predavanje_1;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Pozdrav Andrija!";
+
+    }
+}
